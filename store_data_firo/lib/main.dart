b8aa766573645ec4cee7b733c360ab85f8bc0ac1
sfralfarisi <sfralfarisi@gmail.com>
2025-12-29 10:22:48 +0700

    w13: sOAL 1- 3

---
+++ store_data_firo/lib/main.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+import './model/pizza.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter JSON Demo',
+      theme: ThemeData(
+        primaryColor: Colors.cyan
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String pizzaString = '';
+  List<Pizza> myPizzas = [];
+
+  Future<List<Pizza>> readJsonFile() async {
+    String myString = await DefaultAssetBundle.of(context)
+        .loadString('assets/pizzalist.json');
+    List pizzaMapList = jsonDecode(myString);
+    setState(() {
+      for (var pizza in pizzaMapList) {
+        Pizza myPizza = Pizza.fromJson(pizza);
+        myPizzas.add(myPizza);
+      }
+    });
+    return myPizzas;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    readJsonFile()
+      .then((value) {
+        setState(() {
+          myPizzas = value;
+        });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('JSON'),
+      ),
+      body: ListView.builder(
+        itemCount: myPizzas.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(myPizzas[index].pizzaName),
+            subtitle: Text(myPizzas[index].description),
+          );
+        },
+      )
+    );
+  }
+}
